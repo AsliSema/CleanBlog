903a7d776d02839822b76083fa73b85a042c9665
asliSema <aslisemagultekin@gmail.com>
2021-12-21 22:57:23 +0300

    EJS module uploaded

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
+const { response } = require("express");
 
 const app = express();
 
+app.use(express.static('public'));
 
-app.get("/", (request, responce) => {
-    const blog = {
+app.set("view engine", "ejs");  //Template Engine
+
+app.get("/", (request, response) => {
+/*     const blog = {
         id: 1,
         title: "Blog title",
         description: "Blog description"
     };
-
-    responce.send(blog);
+    responce.send(blog); */
+    //responce.sendFile(path.resolve(__dirname, "temp/index.html"));
+    response.render("index");  //EJS modülü kullanıldığı için
 });
+
+app.get("/about", (request, response) => {
+    response.render("about");
+});
+
+app.get("/add_post", (request, response) => {
+    response.render("add_post");
+});
+
+app.get("/post", (request, response) => {
+    response.render("post");
+});
+
 
 port = 3000;
 
 app.listen(port, ()=>{
     console.log(`Sunucu ${port} de başlatıldı.`);
-})
+});
