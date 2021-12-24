dbd7a633aafcf2f8beb9996f0f7ed3403b337f9f
asliSema <aslisemagultekin@gmail.com>
2021-12-24 15:21:46 +0300

    3rd Section/Posts added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const ejs = require("ejs");
 const path = require("path");
 
-const { response } = require("express");
+const Post = require("./models/Post");
+
+//const { response } = require("express");
 
 const app = express();
 
+//connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
+
+//Template Engine
+app.set("view engine", "ejs");  
+
+//Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.set("view engine", "ejs");  //Template Engine
 
-app.get("/", (request, response) => {
+
+//routes
+app.get("/", async (request, response) => {
 /*     const blog = {
         id: 1,
         title: "Blog title",
@@ -18,7 +32,10 @@ app.get("/", (request, response) => {
     };
     responce.send(blog); */
     //responce.sendFile(path.resolve(__dirname, "temp/index.html"));
-    response.render("index");  //EJS modülü kullanıldığı için
+    const posts = await Post.find();
+    response.render("index", {
+        posts
+    });  //EJS modülü kullanıldığı için
 });
 
 app.get("/about", (request, response) => {
@@ -31,6 +48,11 @@ app.get("/add_post", (request, response) => {
 
 app.get("/post", (request, response) => {
     response.render("post");
+});
+
+app.post("/posts", async (request, response) => {
+    await Post.create(request.body);
+    response.redirect("/");
 });
 
 
